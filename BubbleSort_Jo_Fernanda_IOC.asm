1916bd157062392b945ec1c0e4da086cd4c7d644
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2023-02-11 19:23:11 -0400

    Update BubbleSort_Jo_Fernanda_IOC.asm

--- BubbleSort_Jo_Fernanda_IOC.asm
+++ BubbleSort_Jo_Fernanda_IOC.asm
@@ -1,110 +1,114 @@
 .data
 
+	#Aloca memória para o vetor de 6 elementos (4 bytes x 6 = 24)
 	array: .space 24
-	ordenado: .space 24
+	
+	#variáveis para realizarmos a impressão
 	
 	ola: .asciiz "Olá, entre com 6 números para adicioná-los em um vetor e depois vamos colocalos via bubble sort\n\n"
 	entraNum: .asciiz "Entre com um número: "
 	arrayOrdenado: .asciiz "\nAqui está o array ordenado: "
-	space: .byte ' '
+	space: .byte ' ' #Para colocar o espaço entre os números na impressão final
 	
 	
 	
 .text
 	#main da função
 	.main:
-		li $s3,24
-		jal leituraNumeros
-		li $t2, 0                         # Sets $t2 to 0
-		jal bSort                         # Jumps to bSort
+		li $s3,24			#coloca o tamanho do vetor no registrador s3
+		jal leituraNumeros		#chama a função para o usuário entrar com os números colocando eles em um vetor
+		li $t2, 0 			#inicializa o registrador t2 com 0, para usarmos ele na função bubbleSort
+		jal bubbleSort 			#chama a função para ordenar o vetor usando bubble sort
 		
-		la $a0, arrayOrdenado                # Loads the message
-        	li $v0, 4                         # Prints the message
+		#impressão de mensagem para mostrar o array
+		la $a0, arrayOrdenado 
+        	li $v0, 4 
         	syscall
 		
-		li $t1, 0                         # Loads 0 into $t1
-        	li $t2, 0                         # Loads 0 into $t2
+		li $t1, 0 			#coloca 0 no t1
+        	li $t2, 0 			#coloca 0 no t2
         	
-        	jal printNums                     # Jumps to printNums
-		
-		#finaliza o programa
-		li $v0, 10
-    		syscall
+        	jal printNums 			#chama a função para imprimir o array ordenado
 	
 	
-	
-leituraNumeros:
+	leituraNumeros:
 
-	#imprime mensagem para o usuário de boas vindas
+		#imprime mensagem para o usuário de boas vindas
 		li $v0, 4
 		la $a0, ola
 		syscall
 	
-	move $t0, $zero #inicializa o iterador como 0, o t0 será o iterador "i" de um while
+		move $t0, $zero #inicializa o iterador como 0, o t0 será o iterador "i" de um while
 	
-	lacoRepeticao:
-		#if
-		beq $s3, $t0, acabouNumeros
+		lacoRepeticao:
+			#if t0<s3 (iterador<tamanho do vetor)
+			beq $s3, $t0, acabouNumeros
 		
-		#adiciona mais um ao iterador, como um num inteiro pega 4 bytes adicionamos +4
-		addi $t0, $t0, 4
+			#adiciona mais um ao iterador, como um num inteiro pega 4 bytes adicionamos +4
+			addi $t0, $t0, 4
 		
-		#imprime mensagem para o usuário inserir numero
-		la $a0, entraNum
-		li $v0, 4
-		syscall
+			#imprime mensagem para o usuário inserir numero
+			la $a0, entraNum
+			li $v0, 4
+			syscall
 		
-		#ler o número e colocar no array para ordená-lo ele depois
-		li $v0, 5 #comando para ler inteiro
-		syscall
+			#ler o número e colocar no array para ordená-lo ele depois
+			li $v0, 5 #comando para ler inteiro
+			syscall
 		
-		#move para outro registrador e coloca no array
-		move $t1, $v0
-		sw $t1, array($t0)
+			#move para outro registrador e coloca no array
+			move $t1, $v0
+			sw $t1, array($t0)
 		
-		
-		
-		j lacoRepeticao
+			j lacoRepeticao
 		
 	acabouNumeros:
+		#retorna
 		jr $ra
 		
- # Bubble Sort     
-    bSort:
-        beq $s3, $t2, outLoop             # Branches to outLoop if the count = $s3
-        addi $t5, $t2, 4                  # Adds 4 to the index and saves to $t5
-        lw $t6, array($t2)            # Loads the first index to $t6
-        lw $t7, array($t5)            # Loads the next index to  $t7
-        bge $t7, $t6, skipTheSwap         # Branches to skipTheSwap if they are in the correct order
-        sw $t6, array($t5)            # Swaps the two indexes 
-        sw $t7, array($t2)            # ^
-        addi $t2, $t2, 4                  # Adds 4 to get the next index
-        j bSort                           # Loops back to bSort
     
-    # Skips the swap      
-    skipTheSwap:
-        addi $t2, $t2, 4                  # Does not swap and moves to the next index
-        j bSort                           # Jumps back to bSort
+    	bubbleSort:
+       		beq $s3, $t2, loopExterno 			#if (iterador < tamVetor)
+        	addi $t5, $t2, 4 			#adiciona +4 em t2 e salva em t5
+       		lw $t6, array($t2) 			#carrega o primeiro indice em t6
+        	lw $t7, array($t5) 			#carrega o próximo indice em t7
+        	bge $t7, $t6, pulaTroca 		#pula para a função pulaTroca se já estiver na ordem certa
+        	
+        	#troca os dois índices para ordenar
+        	sw $t6, array($t5)            
+        	sw $t7, array($t2)            
+        	
+        	addi $t2, $t2, 4 			#iterador+4
+        	
+        	j bubbleSort                          #loop volta para bubbleSort até todo o vetor ser percorrido
     
-    # Outer loop
-    outLoop:
-        li $t2, 0                         # Resets $t2 to 0
-        addi $t1, $t1, 1                  # Adds 1 to $t1
-        beq $t1, $s3, acabouNumeros                 # Branches to the end of readNums  
-        j bSort                           # Jumps back to bSort
+    #Função para pular a troca dos indices     
+    pulaTroca:
+        addi $t2, $t2, 4                  		#vai para o próximo índice
+        
+        j bubbleSort                           	#volta para a função bubbleSort
+    
+    #loop externo do bubbleSort
+    loopExterno:
+        li $t2, 0                         		#t2 é 0
+        addi $t1, $t1, 1                  		#t1 + 1
+        beq $t1, $s3, acabouNumeros 			#verifica se vetor já foi todo percorrido
+        
+        j bubbleSort 					#volta para o bubbleSort
     
 
-   # Prints each element of the inputted array    
+   #Função para imprimir todos os elementos do vetor já ordenado
     printNums:
-        beq $s3, $t2, acabouNumeros                # Branches if the index is greater than or equal to count
-	addi $t2, $t2, 4                  # Adds 4 to the current index
-	lw $t4, array($t2)            # Loads sortArray($t0) to $t2
-	move $a0, $t4                     # Moves the current index to the argument
-	li $v0, 1                         # Prints current index
+        beq $s3, $t2, acabouNumeros 			#if(iterador<=tamanhovetor)
+	addi $t2, $t2, 4 				#iterador+4
+	lw $t4, array($t2) 				#carrega o elemento para t4
+	move $a0, $t4 					#move o indice para a0
+	li $v0, 1					#imprime o elemento
 	syscall
-	la $a0, space                     # Prints the space
-        li $v0, 4                         # Loads the space
+	la $a0, space                     		#printa o espaço
+        li $v0, 4                         
         syscall
-	b printNums                       # Jumps back to the loop
+        
+	b printNums                       		#retorna loop
 		
 		
